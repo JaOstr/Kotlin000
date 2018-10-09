15b5b4a855578d3db2523f91a23c5eb5d6169efd
Jacek Ostrowski <jacek.ostrowski@cybercom.com>
2018-10-09 12:43:28 +0200

    Implement Count and Toast

--- app/src/main/java/pl/trrrt/kotlin000/MainActivity.kt
+++ app/src/main/java/pl/trrrt/kotlin000/MainActivity.kt
@@ -2,11 +2,25 @@ package pl.trrrt.kotlin000
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastMe(view: View) {
+        val toast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countMe(view: View) {
+        val cntView = findViewById(R.id.textView) as TextView
+        val cnt = Integer.parseInt(cntView.text.toString()) + 1
+        cntView.text = cnt.toString()
     }
 }
